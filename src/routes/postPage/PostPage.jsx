2171341d813673758ac74495fbeb1b35c765e109
Narotam-Mishra <narotam21jan@gmail.com>
2025-03-12 22:57:48 +0530

    react-pinterest-added_react-router-for_each_component_p1_2.1

---
+++ src/routes/postPage/PostPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PostPage.css';
+
+const PostPage = () => {
+  return (
+    <div className='PostPage'>PostPage</div>
+  )
+}
+
+export default PostPage
---
+++ src/routes/profilePage/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProfilePage.css';
+
+const ProfilePage = () => {
+  return (
+    <div className='profilePage'>ProfilePage</div>
+  )
+}
+
+export default ProfilePage
