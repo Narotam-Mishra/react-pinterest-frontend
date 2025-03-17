5ebaabd183a62c12cd7b0426a3bb16e3749def97
Narotam-Mishra <narotam21jan@gmail.com>
2025-03-17 06:27:51 +0530

    react-pinterest-added_added_image_optimization_using_ImageKitIO_p3_completed_2.4

--- src/components/galleryItems/GalleryItems.jsx
+++ src/components/galleryItems/GalleryItems.jsx
@@ -1,27 +1,19 @@
 import React from 'react'
 import './GalleryItems.css';
 import { Link } from 'react-router'
-import { IKImage } from 'imagekitio-react';
+import Image from '../image/Image';
 
 const GalleryItems = ({ item }) => {
+  const optimizedHeight = (372 * item.height) / item.width;
+
   return (
     <div
       className="galleryItem"
       style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
     >
       {/* <img src={item.media} alt="" /> */}
-      <IKImage
-        urlEndpoint={import.meta.env.VITE_URL_IK_ENDPOINT}
-        path={item.media}
-        transformation={[
-          {
-            width: 372,
-          },
-        ]}
-        alt='test'
-        loading='lazy'
-        lqip={{ active: true, quality: 20 }}
-      />
+      <Image path={item.media} alt="" w={372} h={optimizedHeight} />
+      
       <Link to={`/pin/${item.id}`} className="overlay" />
       <button className="saveButton">Save</button>
       <div className="overlayIcons">
