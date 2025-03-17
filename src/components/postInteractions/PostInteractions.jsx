d558c2189f0d6304440779b054936a088ca6f2c3
Narotam-Mishra <narotam21jan@gmail.com>
2025-03-17 13:32:50 +0530

    react-pinterest-single_post_page_design_p1_3.1

---
+++ src/components/postInteractions/PostInteractions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PostInteractions.css';
+
+const PostInteractions = () => {
+  return (
+    <div className='postInteractions'>PostInteractions</div>
+  )
+}
+
+export default PostInteractions
--- src/routes/postPage/PostPage.jsx
+++ src/routes/postPage/PostPage.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './PostPage.css';
+import Image from '../../components/image/Image';
+import PostInteractions from '../../components/postInteractions/PostInteractions';
+import { Link } from 'react-router';
 
 const PostPage = () => {
   return (
-    <div className='PostPage'>PostPage</div>
+    <div className='postPage'>
+      <div className="postContainer">
+        <div className="postImg">
+          <Image path="/pins/pin1.jpeg" alt="" w={736} />
+        </div>
+        <div className="postDetails">
+          <PostInteractions />
+          <Link to="/john" className='postUser'>
+            <Image path="/general/noAvatar.png" />
+            <span>John Doe</span>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
