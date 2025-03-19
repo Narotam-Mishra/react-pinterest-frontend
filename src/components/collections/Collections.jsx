6c85e6d1a250e0193be34f1de8f90d5d92c9567c
Narotam-Mishra <narotam21jan@gmail.com>
2025-03-19 20:03:09 +0530

    react-pinterest-added_UserProfilePageDesign_3.5

---
+++ src/components/collections/Collections.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import './Collections.css';
+import Image from '../image/Image';
+
+const Collections = () => {
+  return (
+    <div className="collections">
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+
+      {/* COLLECTION */}
+      <div className="collection">
+        <Image path="/pins/pin1.jpeg" alt="" />
+        <div className="collectionInfo">
+          <h1>Minimalist Bedrooms</h1>
+          <span>12 Pins . 1w</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Collections
--- src/routes/profilePage/ProfilePage.jsx
+++ src/routes/profilePage/ProfilePage.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProfilePage.css';
+import Image from '../../components/image/Image';
+import Collections from '../../components/collections/Collections';
+import Gallery from '../../components/gallery/Gallery';
 
 const ProfilePage = () => {
+  const [type, setType] = useState("saved")
   return (
-    <div className='profilePage'>ProfilePage</div>
-  )
+    <div className="profilePage">
+      <Image
+        className="profileImg"
+        path={"/general/noAvatar.png"}
+        w={100}
+        h={100}
+        alt=""
+      />
+      <h1 className="profileName">John Doe</h1>
+      <span className="profileUsername">@johndoe</span>
+      <div className="followCounts">10 followers . 20 followings</div>
+      <div className="profileInteractions">
+        <Image path="/general/share.svg" alt="" />
+        <div className="profileButtons">
+          <button>Message</button>
+          <button>Follow</button>
+        </div>
+        <Image path="/general/more.svg" alt="" />
+      </div>
+      <div className="profileOptions">
+        <span onClick={() => setType("created")} className={type === "created" ? "active" : ""}>Saved</span>
+        <span onClick={() => setType("saved")} className={type === "saved" ? "active" : ""}>Created</span>
+      </div>
+      {
+        type === "created" ? <Gallery /> : <Collections />
+      }
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
