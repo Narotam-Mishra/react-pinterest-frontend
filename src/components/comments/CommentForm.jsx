d19db92210b543458a99c81692c103250db1e625
Narotam-Mishra <narotam21jan@gmail.com>
2025-04-03 23:17:35 +0530

    react-pinterest_updated_comments_form_p1_2.9

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -3,16 +3,28 @@ import { useState } from "react";
 
 const CommentForm = () => {
   const [open, setOpen] = useState(false);
+  const [desc, setDesc ] = useState("");
+
+  const handleEmojiClick = (emojiObj) => {
+    setDesc((prev) => prev +" "+emojiObj.emoji);
+    setOpen(false);
+  }
+
   return (
     <>
       {/* comment input form */}
       <form className="commentForm">
-        <input type="text" placeholder="Add a comment" />
+        <input
+          type="text"
+          placeholder="Add a comment"
+          onChange={(e) => setDesc(e.target.value)}
+          value={desc}
+        />
         <div className="emoji">
           <div onClick={() => setOpen((prev) => !prev)}>😊</div>
           {open && (
             <div className="emojiPicker">
-              <EmojiPicker />
+              <EmojiPicker onEmojiClick={handleEmojiClick} />
             </div>
           )}
         </div>
