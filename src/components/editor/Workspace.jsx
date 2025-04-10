d2f65a90e13e1aee127de936620aaf19aaf84ae7
Narotam-Mishra <narotam21jan@gmail.com>
2025-04-10 16:35:42 +0530

    eact-pinterest_added_Workspace_for_Pin_design_p3_3.8

--- src/components/editor/Workspace.jsx
+++ src/components/editor/Workspace.jsx
@@ -1,8 +1,25 @@
+import useEditorStore from "../../utils/editorStore"
 
-const Workspace = () => {
+const Workspace = ({ previewImg }) => {
+  const { textOptions, setTextOptions } = useEditorStore()
   return (
-    <div className="workspace">Workspace</div>
-  )
+    <div className="workspace">
+      <div className="canvas">
+        <img src={previewImg.url} alt="" />
+        {textOptions.text && (
+          <div className="">
+            <input
+              type="text"
+              value={textOptions.text}
+              onChange={(e) =>
+                setTextOptions({ ...textOptions, text: e.target.value })
+              }
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Workspace
--- src/utils/editorStore.js
+++ src/utils/editorStore.js
@@ -1,8 +1,25 @@
 import { create } from 'zustand';
 
 const useEditorStore = create((set) => ({
-    selectedLayer: "canvas",
-    setSelectedLayer: (newLayer) => set({ selectedLayer: newLayer }),
+  selectedLayer: "canvas",
+  textOptions: {
+    text: "",
+    fontSize: 48,
+    color: "#000000",
+    top: 0,
+    left: 0,
+  },
+  setSelectedLayer: (newLayer) => set({ selectedLayer: newLayer }),
+  setTextOptions: (newOptions) => ({ textOptions: newOptions }),
+  addText: () => ({
+    textOptions: {
+      text: "Add text",
+      fontSize: 48,
+      color: "#000000",
+      top: 0,
+      left: 0,
+    },
+  }),
 }));
 
 export default useEditorStore;
